51921623fecebe1acf0bf66b2eae2954f9282b1e
sumit yewale <sumit.thaliatechnologies.com>
2023-10-06 15:39:09 +0530

    Typescript notations added

---
+++ @types/csvbox__react.d.ts
@@ -0,0 +1,39 @@
+import React, { PropsWithChildren } from 'react';
+declare module '@csvbox/react' {
+    interface CSVBoxImporterProps {
+        lazy?: boolean;
+        loadStarted?:() => void;
+        user: any;
+        dynamicColumns?: any;
+        options?: any;
+        onReady?:() => void;
+        onImport:(type:boolean, metadata?:any) => void;
+        onSubmit?:(metadata:any) => void;
+        onClose?:() => void;
+        licenseKey: string;
+        dataLocation?: string;
+        customDomain?: string;
+        language?: string;
+        render?:(launch:any, isLoading:boolean) => void
+    }
+    
+    export class CSVBoxButton extends React.Component<PropsWithChildren<CSVBoxImporterProps>>{
+        constructor(props: CSVBoxImporterProps);
+
+        holder: React.RefObject<HTMLDivElement>;
+        isModalShown: boolean;
+        shouldOpenModalOnReady: boolean;
+        uuid: string;
+        iframe: HTMLIFrameElement | null;
+
+        openModal(): void;
+
+        generateUuid(): string;
+
+        componentDidMount(): void;
+
+        initImporter(): void;
+
+        enableInitator(): void;
+    }
+}
